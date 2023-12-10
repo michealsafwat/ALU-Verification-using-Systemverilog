8c437c240633a5d3d7c55e002c9f7108d45f9d9a
Micheal Safwat <51376282+michealsafwat@users.noreply.github.com>
2023-12-10 23:20:26 +0200

    Update generator.svh

--- generator.svh
+++ generator.svh
@@ -11,7 +11,7 @@ class generator;
     trans = new();
     this.mbx = mbx;
   endfunction
-  
+
   task run();
     repeat (count) begin
       assert (trans.randomize)
@@ -22,19 +22,19 @@ class generator;
       else $error("[GEN] Both enables for mode 'a' and mode 'b' are high!");
 
       illega_a_1 :
-      assert (((trans.alu_enable_a && (trans.alu_op_a == 2'b0)) && (trans.alu_in_b == 8'b0)) != 1)
+      assert (((trans.alu_enable_a && trans.alu_enable && (trans.alu_op_a == 2'b0)) && (trans.alu_in_b == 8'b0)) != 1)
       else $error("[GEN] illegal value in mode 'a'!");
 
       illega_a_2 :
-      assert (((trans.alu_enable_a && (trans.alu_op_a == 2'b01)) && (trans.alu_in_b == 8'h03) && (trans.alu_in_a == 8'hff)) != 1)
+      assert (((trans.alu_enable_a && trans.alu_enable && (trans.alu_op_a == 2'b01)) && (trans.alu_in_b == 8'h03) && (trans.alu_in_a == 8'hff)) != 1)
       else $error("[GEN] illegal value in mode 'a'!");
 
       illega_b_1 :
-      assert (((trans.alu_enable_b && (trans.alu_op_b == 2'b01)) && (trans.alu_in_b == 8'h03)) != 1)
+      assert (((trans.alu_enable_b && trans.alu_enable && (trans.alu_op_b == 2'b01)) && (trans.alu_in_b == 8'h03)) != 1)
       else $error("[GEN] illegal value in mode 'b'!");
 
       illega_b_2 :
-      assert (((trans.alu_enable_b && (trans.alu_op_b == 2'b10)) && (trans.alu_in_a == 8'hf5)) != 1)
+      assert (((trans.alu_enable_b && trans.alu_enable && (trans.alu_op_b == 2'b10)) && (trans.alu_in_a == 8'hf5)) != 1)
       else $error("[GEN] illegal value in mode 'b'!");
 
       trans.display("GEN");
